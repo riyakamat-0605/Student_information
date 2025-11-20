1e0ca2b1b5ed5774bd32034744dd8094039a1490
riyakamat-0605 <riyakamat2006@gmail.com>
2025-11-20 16:14:22 +0530

    Update student.py

--- student.py
+++ student.py
@@ -6,8 +6,8 @@ if len(sys.argv) != 3:
 
 script_name = sys.argv[0]
 name = sys.argv[1]
-roll_no = sys.argv[2]
+rollno = sys.argv[2]
 
 print("script name: ", script_name)
 print("name: ", name)
-print("roll no: ", roll_no)
+print("roll no: ", rollno)
